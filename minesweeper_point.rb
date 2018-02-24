4fa38308c04fa2e8e8aae20ebf1142c3d02a5c79
kurttomlinson <kurttomlinson@gmail.com>
2018-02-24 15:17:32 -0600

    don't keep throwing away objects

--- minesweeper_point.rb
+++ minesweeper_point.rb
@@ -2,10 +2,12 @@ class MinesweeperPoint
   attr_accessor :covered
   attr_accessor :bomb_present
   attr_accessor :adjacent_bomb_count
+
+  @@rng = Random.new
+
   def initialize(bomb_likelihood_percent: 10)
     self.covered = true
-    rng = Random.new
-    self.bomb_present = (rng.rand(100) < bomb_likelihood_percent)
+    self.bomb_present = (@@rng.rand(100) < bomb_likelihood_percent)
   end
   def bomb_present?
     bomb_present
