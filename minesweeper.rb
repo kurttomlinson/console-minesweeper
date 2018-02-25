454b5c4cf4eed6b87c1a9ac6ca7bead85ba3e839
Kurt Tomlinson <kurttomlinson@gmail.com>
2018-02-25 17:37:06 -0600

    move play code into a game method

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,32 +1,3 @@
 require './minesweeper_game'
 
-puts "board width?"
-width = gets.to_i
-
-puts "board height?"
-height = gets.to_i
-
-puts "bomb likelihood percent? (0 to 100)"
-bomb_likelihood = gets.to_i
-
-game = MinesweeperGame.new(height: height, width: width, bomb_likelihood_percent: bomb_likelihood)
-
-while game.status == :in_progress
-  puts game.board
-  puts "x?"
-  column = gets.to_i
-  puts "y?"
-  row = gets.to_i
-  game.make_move(row: row, column: column)
-end
-
-game.board.points.flatten.each do |point|
-  point.covered = false
-end
-puts game.board
-
-if game.status == :win
-  puts "you won!"
-elsif game.status == :lose
-  puts "you lost."
-end
+MinesweeperGame.new.play
--- minesweeper_game.rb
+++ minesweeper_game.rb
@@ -1,14 +1,45 @@
 require './minesweeper_board'
+require 'pry'
 
 class MinesweeperGame
   attr_accessor :board
   attr_accessor :status
 
-  def initialize(height: 10, width: 10, bomb_likelihood_percent: 10)
-    @height = height
-    @width = width
-    @board = MinesweeperBoard.new(height: height, width: width, bomb_likelihood_percent: bomb_likelihood_percent)
+  def initialize()
     @status = :in_progress
+  end
+
+  def play
+    puts "board width?"
+    @width = gets.to_i
+
+    puts "board height?"
+    @height = gets.to_i
+
+    puts "bomb likelihood percent? (0 to 100)"
+    @bomb_likelihood_percent = gets.to_i
+
+    @board = MinesweeperBoard.new(height: @height, width: @width, bomb_likelihood_percent: @bomb_likelihood_percent)
+
+    while @status == :in_progress
+      puts @board
+      puts "x?"
+      column = gets.to_i
+      puts "y?"
+      row = gets.to_i
+      make_move(row: row, column: column)
+    end
+
+    @board.points.flatten.each do |point|
+      point.covered = false
+    end
+    puts @board
+
+    if @status == :win
+      puts "you won!"
+    elsif @status == :lose
+      puts "you lost."
+    end
   end
 
   def make_move(row: 0, column: 0)
