c88b64150d6a8bfcee92f5daff99d32ef14d27ad
Kurt Tomlinson <kurttomlinson@gmail.com>
2018-02-27 12:24:55 -0600

    move play out of the game class

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,3 +1,37 @@
 require './minesweeper_game'
 
-MinesweeperGame.new.play
+def play
+  puts "board width?"
+  width = gets.to_i
+
+  puts "board height?"
+  height = gets.to_i
+
+  puts "bomb likelihood percent? (0 to 100)"
+  bomb_likelihood_percent = gets.to_i
+
+  game = MinesweeperGame.new(height: height, width: width, bomb_likelihood_percent: bomb_likelihood_percent)
+
+  while game.status == :in_progress
+    puts game.board
+    puts "x?"
+    column = gets.to_i
+    puts "y?"
+    row = gets.to_i
+    begin
+      game.make_move(row: row, column: column)
+    rescue MinesweeperBoard::InvalidPointError
+      puts "invalid move location"
+    end
+  end
+
+  puts game.board
+
+  if game.status == :win
+    puts "you won!"
+  elsif game.status == :lose
+    puts "you lost."
+  end
+end
+
+play
